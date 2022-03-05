d04abc8aae34db6f6518b1ca8f6b28523fcf49c0
Przemysław Piasecki <zpzm@pm.me>
2022-03-05 06:45:49 +0100

    removed dead code

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <cryptopp/hex.h>
 #include <sstream>
 #include <wx/wx.h>
-//#include <filesystem>
 
 #include "cryptography.hpp"
 #include "mainwindow.hpp"
@@ -21,27 +20,3 @@ bool MyApp::OnInit(){
     frame->Show(true);
     return true;
 }
-
-
-//namespace fs = std::filesystem;
-/*
-int main (){
-
-
-	fs::path ipath("abc/encrypted.gif");
-	fs::path opath;
-	opath  = ipath.parent_path();
-	opath /= ipath.stem();
-	opath += "_decr";
-	opath += ipath.extension();
-
-	std::cout << opath.string();
-
-	std::ifstream input (ipath.native(), std::fstream::binary|std::fstream::in );
-	//std::stringstream buf;
-	std::ofstream output(opath.native(), std::fstream::binary|std::fstream::out);
-
-	if (input.good()){
-		decrypt(input,output);
-	}
-}*/
